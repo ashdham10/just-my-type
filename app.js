646c80846f810b5ff16007536bfc03c07a32f5c1
ashdham10 <ashleydenham10@gmail.com>
2018-09-19 17:03:44 -0500

    sentences

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 $(document).ready(function(){
     $('#keyboard-upper-container').hide();
 });
-
+//Shift function
 $(document).keydown(function(e){ 
 if (e.keyCode == 16) {
     $('#keyboard-upper-container').show();
     $('#keyboard-lower-container').hide();
-}
+    }
 });
-$(document).keyup(function(){
+$(document).keyup(function(e){
+    if (e.keyCode == 16){
     $('#keyboard-lower-container').show();
     $('#keyboard-upper-container').hide();
+    }
 }); 
+//Highlighting pressed key
+$(document).keypress(function(e){
+    $(`#${e.key.charCodeAt()}`).css('background', 'yellow');
+   
+});
+$(document).keyup(function(e){
+    $(`#${e.key.charCodeAt()}`).css('background', '');
+});
 
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
-$(document).keypress(function(k){
-    $('.key').css('background', 'yellow');
-})
-$(document).keyup(function(){
-    $('.key').css('background', '');
-})
+$('#sentence').append(sentences[0]);
+
